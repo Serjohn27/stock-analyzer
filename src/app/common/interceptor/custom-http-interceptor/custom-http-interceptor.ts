bf71398fd6b6a0a6675caa0a99f8c1f8400b38de
fikret <serjohn2727@gmail.com>
2021-02-26 12:43:22 -0500

    moved error handling to interceptor

--- src/app/common/interceptor/custom-http-interceptor/custom-http-interceptor.ts
+++ src/app/common/interceptor/custom-http-interceptor/custom-http-interceptor.ts
@@ -1,7 +1,7 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { finalize, tap } from 'rxjs/operators';
 
 @Injectable()
@@ -14,11 +14,28 @@ export class CustomHttpInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     this.spinner.show()
     this.count++;
-    return next.handle(req).pipe(tap(event => console.log(event),error => console.log(error)), 
+    return next.handle(req).pipe(tap(event => console.log(event),error => this.handleError(error)), 
           finalize(() => { 
              this.count--;
              if (this.count == 0) this.spinner.hide()
           })
       );
   }
+
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
 }
